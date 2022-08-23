1a3c37aca2e418360ca7ef0ee3fdff13d25ed53b
pram11 <an6207@gmail.com>
2022-08-23 22:58:07 +0900

    네트워크 요청 API 작성

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,45 @@
-import { ListFormat } from "typescript"
 
 export type MenuItemType={
     id:number,
     name:String
 }
 
+export type CallRequestType={
+    type:number,
+    visit_reason:String
+}
+
+
+
+class NetworkError extends Error{
+    constructor(msg: string) {
+        super(msg);
+
+        // Set the prototype explicitly.
+        Object.setPrototypeOf(this, NetworkError.prototype);
+    }
+
+}
+
+export async function createMenu(param:MenuItemType){
+    let response = await fetch("localhost:8000/doorbell/category",{
+        method:"POST",
+        body:JSON.stringify(param)
+    })
+    if (!response.ok){
+        throw new NetworkError("메뉴 생성중 네트워크 에러가 발생하였습니다.");
+    }
+}
+
 export async function getMenuList():Promise<MenuItemType[]>
 {
-    // fetch()
+
+    // let response = await fetch("localhost:8000/doorbell/category",{
+    //     method:"GET"
+    // })
+    // return response.json()
+    
+
     return new Promise((resolve,reject)=>{resolve([
         {
             id:0,
@@ -23,6 +55,13 @@ export async function getMenuList():Promise<MenuItemType[]>
         }
     ])})
 }
-export function createCallRequest(){
-
+export async function createCallRequest(param:CallRequestType){
+    let response = await fetch("localhost:8000/doorbell/visit",{
+        method:"POST",
+        body:JSON.stringify(param)
+    })
+    if (!response.ok){
+        throw new NetworkError("메뉴 생성중 네트워크 에러가 발생하였습니다.");
+    }
 }
+
